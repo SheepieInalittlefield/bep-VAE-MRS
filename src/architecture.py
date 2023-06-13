Implemented basic t-SNE Fixed averaging optimized models

src/architecture.py
@@ -95,30 +95,20 @@ class DenseEncoder(BaseEncoder):
         self.latent_dim = args.latent_dim
 
         self.layers = nn.Sequential(
-            nn.Conv1d(self.n_channels, 2, 3, 2, padding=1),
-            nn.BatchNorm1d(2),
-            nn.LeakyReLU(),
-            nn.Conv1d(2, 4, 3, 2, padding=1),
-            nn.BatchNorm1d(4),
-            nn.LeakyReLU(),
-            nn.Conv1d(4, 8, 3, 2, padding=1),
-            nn.BatchNorm1d(8),
-            nn.LeakyReLU(),
-            nn.Conv1d(8, 16, 3, 2, padding=1),
-            nn.BatchNorm1d(16),
-            nn.LeakyReLU(),
             nn.Flatten(),
             nn.Linear(2048, 1024),
             nn.BatchNorm1d(1024),
-            nn.ReLU(),
-
+            nn.LeakyReLU(),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(),
         )
 
-        self.log_var = nn.Linear(1024, self.latent_dim)
-        self.embedding = nn.Linear(1024, self.latent_dim)
+        self.log_var = nn.Linear(512, self.latent_dim)
+        self.embedding = nn.Linear(512, self.latent_dim)
 
     def forward(self, x: torch.Tensor) -> ModelOutput:
-        h1 = self.layers(x).reshape([x.shape[0], 1024])  # x.shape[0] = batch size
+        h1 = self.layers(x).reshape([x.shape[0], 512])  # x.shape[0] = batch size
         output = ModelOutput(
             embedding=self.embedding(h1),
             log_covariance=self.log_var(h1)
@@ -133,21 +123,21 @@ class DenseDecoder(BaseDecoder):
         self.latent_dim = args.latent_dim
         self.n_channels = 1
 
-        self.fc = nn.Linear(self.latent_dim, 1024)
 
         self.layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024, 1024),
-            nn.BatchNorm1d(1024),
-            nn.Linear(1024, 2048),
+            nn.Linear(self.latent_dim, 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 2048),
             nn.BatchNorm1d(2048),
-            nn.Identity()
+            nn.Identity(),
         )
 
     def forward(self, z: torch.Tensor) -> ModelOutput:
-        h1 = self.fc(z).reshape(z.shape[0], 1024, 1)
         output = ModelOutput(
-            reconstruction=self.layers(h1)
+            reconstruction=self.layers(z)
         )
         return output
 
@@ -159,19 +149,17 @@ class DenseDiscriminator(BaseDiscriminator):
         self.latent_dim = args.latent_dim
         self.n_channels = 1
 
-        self.fc = nn.Linear(self.latent_dim, 1024)
         self.layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024, 2048),
-            nn.BatchNorm1d(2048),
+            nn.Linear(2048, 512),
             nn.LeakyReLU(),
-            nn.Linear(2048, 2),
+            nn.Linear(512,256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 1),
             nn.Sigmoid()
         )
 
-    def forward(self, x: torch.Tensor) -> ModelOutput:
-        h1 = self.fc(x).reshape(x.shape[0], 1024, 1)
+    def forward(self, x: torch.Tensor, output_layer_levels=3) -> ModelOutput:
         output = ModelOutput(
-            adversarial_cost=self.layers(h1)
+            adversarial_cost=self.layers(x)
         )
         return output

src/data.py
@@ -52,39 +52,65 @@ def load_mrs_simulations():
     return y_train, y_eval, ppm
 
 
-def pre_process_real(gt_fids, n_fids):
-    gt_spec = np.fft.fftshift(np.fft.ifft(gt_fids, axis=1), axes=1)
-    gt_diff_spec = np.real(gt_spec[:, :, 1, :] - gt_spec[:, :, 0, :])
+def pre_process_train(gt_fids, n_samples):
+    spec = np.fft.fftshift(np.fft.ifft(gt_fids, axis=1), axes=1)
+    spec_on = spec[:,:,0,:160]
+    spec_on = np.array(np.split(spec_on, [40,80,120], axis=2))
+    spec_on = np.concatenate([i for i in spec_on]).mean(axis=2, keepdims=True)
+    spec_off = spec[:,:,1,:160]
+    spec_off = np.array(np.split(spec_off, [40,80,120], axis=2))
+    spec_off = np.concatenate([i for i in spec_off]).mean(axis=2, keepdims=True)
 
-    y = gt_diff_spec[:, :, :n_fids]
-    y = y.swapaxes(0,2).swapaxes(1,2)
-    y = np.reshape(y, [y.shape[0]*y.shape[1], 1, 2048])
+    y = np.real(spec_off - spec_on)
+    y = y.swapaxes(1,2)
+    #y = y.swapaxes(0,2).swapaxes(1,2)
+    #y = np.reshape(y, [y.shape[0]*y.shape[1], 1, 2048])
+    print('test: ', y.shape)
     y_max = y.max(axis=2, keepdims=True)
     y_mean = y.mean(axis=2, keepdims=True)
     y = (y - y_mean) / (y_max - y_mean)
-    y = torch.from_numpy(y)  # at this point the shape of y is [n_samples ,1,2048]
+    y = torch.from_numpy(y)  # at this point the shape of y is [12, 1, 2048]
+    return y
+
+
+def pre_process_test(gt_fids):
+    spec = np.fft.fftshift(np.fft.ifft(gt_fids, axis=1), axes=1)
+    test = np.transpose(spec, (0,3,2,1))
+    print(test.shape)
+    test = np.real(np.reshape(test, (test.shape[0]*test.shape[1], test.shape[2], test.shape[3])))
+    print(test.shape)
+    test_on = test[:,0,:]
+    print(test_on.shape)
+    spec_on = spec[:,:,0,:40].mean(axis=2, keepdims=True)
+    spec_off = spec[:,:,1,:40].mean(axis=2, keepdims=True)
+    y = np.real(spec_off - spec_on)
+    y = y.swapaxes(1, 2)
+    y_max = y.max(axis=2, keepdims=True)
+    y_mean = y.mean(axis=2, keepdims=True)
+    y = (y - y_mean) / (y_max - y_mean)
+    y = torch.from_numpy(y)  # at this point the shape of y is [12, 1, 2048]
     return y
 
 def load_train_real():
     with h5py.File("../MRS_data/track_02_training_data.h5") as hf:
         gt_fids = hf["transient_fids"][()]  # shape (12, 2048, 2, 160)
         ppm = hf["ppm"][()][:1]
-    y = pre_process_real(gt_fids, 160)
-    y_train = y[:1800]
-    y_eval = y[1800:]
+    y = pre_process_train(gt_fids, 48)
+    y_train = y[:40]
+    y_eval = y[40:]
     return y_train, y_eval, ppm
 
 
 def load_test_real():
     with h5py.File("../MRS_data/track_02_test_data.h5") as hf:
         gt_fids = hf["transient_fids"][()]
-    y = pre_process_real(gt_fids, 40)
+    y = pre_process_test(gt_fids)
     return y
 
 
 def load_target():
     with h5py.File("../MRS_data/track_02_training_data.h5") as hf:
-        target = hf["target_spectra"][()]
+        target = hf["target_spectra"][()] # shape (12, 1, 2048)
         ppm = hf["ppm"][()][:1]
 
     y = target

src/main.py
@@ -3,30 +3,28 @@ from data import load_target, load_test_real, load_train_real
 from utilities.hyperparameter import *
 import torch
 from pythae.samplers import GaussianMixtureSamplerConfig, GaussianMixtureSampler
-from src.utilities.visualization import plot_2d_latent_interpolation, plot_static_latent_interpolation, sample_model, show_generated_data
-
-
+from src.utilities.visualization import plot_2d_latent_interpolation, plot_static_latent_interpolation, sample_model, show_generated_data, plot_some_generated_samples
+from training import get_last_trained
+import matplotlib.pyplot as plt
 def main():
     parameters = gen_parameters(
-        output_dir='../dis_beta_VAE',
-        lr=2e-3,
+        output_dir='../linflow_VAE',
+        lr=1e-3,
         batch_size=16,
-        epochs=100,
-        dim=8,
-        beta=250,
-        C=20,
-        warmup=250,
+        epochs=2000,
+        dim=256,
+        flows=['Radial', 'Radial'],
         trainer='base',
-        architecture='convolutional',
+        architecture='dense',
         optimizer='AdamW',
-        weight_decay=2e-2,
-        beta1=0.92,
-        beta2=0.999,
+        weight_decay=2e-3,
+        beta1=0.90,
+        beta2=0.998,
         disc='mlp',
         data='real'
     )
 
-    trained_model, mse = train_model(dis_beta_VAE_config, parameters)
+    trained_model, mse = train_model(linflow_VAE_config, parameters)
 
     print(f"MSE: {mse}")
 
@@ -34,27 +32,31 @@ def main():
     gen_data = sample_model(trained_model).cpu()
     if gen_data.shape[1] == 1:
         gen_data = gen_data[:, 0, :]
-    target = target.mean(axis=0)
+    target = target[0]
     test_data = load_test_real()
     show_generated_data(gen_data, test_data, target, ppm)
 
 
 def test_random_stuff():
-    from training import get_last_trained
 
-    model_path = '../dis_beta_VAE'
+    model_path = '../custom_VAE'
     model = get_last_trained(model_path)
+    train, eval, ppm = load_train_real()
+
     # target, ppm = load_target()
     # test_data = load_test_real()
-    # gen_data = sample_model(model)
-    # gen_data = gen_data.cpu()
+    gen_data = sample_model(model)
+    #gen_data = gen_data.cpu()
     # if gen_data.shape[1] == 1:
     #     gen_data = gen_data[:, 0, :]
     # target = target.mean(axis=0)
     # show_generated_data(gen_data, test_data, target, ppm)
+
+
     test_data = load_test_real()
     target, ppm = load_target()
-    plot_static_latent_interpolation(model, test_data, ppm)
+    plot_some_generated_samples(gen_data.cpu(), ppm)
+    #plot_static_latent_interpolation(model, test_data, ppm)
 
 
 def parameter_search():
@@ -65,7 +67,3 @@ if __name__ == '__main__':
     #parameter_search()
     main()
     test_random_stuff()
-
-# MSE: 0.0001698292866272024
-# Train loss: 0.9325
-# Eval loss: 2.6887

src/training.py
@@ -8,7 +8,7 @@ from src.architecture import ConvolutionalEncoder, ConvolutionalDecoder, DenseEn
     DenseDiscriminator
 from src.config import gen_parameters
 from src.utilities.visualization import sample_model, mse
-from torch import mean
+from numpy import mean
 import time
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -101,6 +101,9 @@ def train_model(getter, parameters):
     end_time = time.time()
     print("Total Time trained: {}".format(end_time - start_time))
     gen_data = sample_model(trained_model)
-    test_data_mean = mean(test_dataset, dim=0)
-    error = mse(gen_data, test_data_mean)
+    error = []
+    for i in range(test_dataset.shape[0]):
+        for j in range(gen_data.shape[0]):
+            error.append(mse(gen_data[j], test_dataset[i]))
+    error = mean(error)
     return trained_model, float(error)

src/utilities/visualization.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pythae.samplers
 import torch
+from random import randint
 
-
-def sample_model(trained_model, Sampler=pythae.samplers.NormalSampler, config=None, train_data=None, n=960):
+def sample_model(trained_model, Sampler=pythae.samplers.NormalSampler, config=None, train_data=None, n=480):
     if config:
         sampler = Sampler(trained_model, config)
         sampler.fit(train_data)
@@ -29,6 +29,19 @@ def plot_input_interpolation(trained_model, ppm, start, stop):
     plt.show()
 
 
+def plot_some_generated_samples(gen_data, ppm):
+    fig, ax = plt.subplots(4, 4, figsize=(16, 16))
+    n = 0
+    for i in ax:
+        for j in i:
+            j.plot(ppm[0], gen_data[n])
+            j.invert_xaxis()
+            j.set_ylim(-1, 1)
+            n += 1
+    fig.show()
+    return 1
+
+
 def plot_2d_latent_interpolation(model, ppm):
     x = np.linspace(-5, 5, 5)
     y = np.linspace(-5, 5, 5)
@@ -42,21 +55,21 @@ def plot_2d_latent_interpolation(model, ppm):
             ax[i][j].set_title(f"x = {x[i]}, y = {y[j]}")
     plt.show()
 
+
 def plot_static_latent_interpolation(model, test_data, ppm):
     x = np.linspace(-10, 10, 8)
     num_rows = 8
-    fig, ax = plt.subplots(nrows=num_rows, ncols=8, figsize=(20,20))
-    test_data = test_data.type(torch.float32)
+    fig, ax = plt.subplots(nrows=num_rows, ncols=8, figsize=(30, 20))
+    test_data = test_data.type(torch.float32).to("cuda:0")
     for i in range(num_rows):
         for j in range(len(x)):
-            embedding = model.embed(test_data[:1, :, :])
+            embedding = model.embed(test_data[:2, :, :])
             embedding[0][i] = x[j]
-            gen_data = model.decoder(embedding).reconstruction
-            ax[i][j].plot(ppm[0], gen_data[0][0].cpu().detach())
+            gen_data = model.decoder(embedding[:1]).reconstruction
+            ax[i][j].plot(ppm[0], gen_data[0].cpu().detach())
             ax[i][j].invert_xaxis()
-            ax[i][j].set_ylim(-1, 0.3)
+            ax[i][j].set_ylim(-1, 1)
     plt.show()
-
 
 
 def plot_sample(sample, ppm):
@@ -77,24 +90,23 @@ def show_generated_data(gen_data, real_data, target_spectrum, ppm):
     if type(real_data) != np.ndarray:
         real_data = real_data.numpy()
     ppm = ppm[0]
-
-    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-    ax[0][0].plot(ppm, gen_data[0])
-    ax[0][1].plot(ppm, target_spectrum)
+    sample = randint(0, 480)
+    fig, ax = plt.subplots(2, 2, figsize=(15, 10))
+    ax[0][0].plot(ppm, gen_data[sample])
+    ax[0][1].plot(ppm, real_data[0][0])
     ax[1][0].plot(ppm, gen_data[:].mean(axis=0))
-    ax[1][1].plot(ppm, real_data[:][0].mean(axis=0))
+    ax[1][1].plot(ppm, real_data.mean(axis=0)[0])
     ax[0][0].set_title('generated data, single sample')
-    ax[0][1].set_title('target spectrum')
+    ax[0][1].set_title('real data, single sample')
     ax[1][0].set_title('generated data, mean of 480 samples')
-    ax[1][1].set_title('real data, mean of 960 samples')
-    ax[0][0].invert_xaxis()
-    ax[0][0].set_xlabel("ppm")
-    ax[0][1].invert_xaxis()
-    ax[0][1].set_xlabel("ppm")
-    ax[1][0].invert_xaxis()
-    ax[1][0].set_xlabel("ppm")
-    ax[1][1].invert_xaxis()
-    ax[1][1].set_xlabel("ppm")
+    ax[1][1].set_title('real data, mean of 480 samples')
+    for i in ax:
+        for j in i:
+            j.set_ylim(-1,1)
+            j.set_xlim(0,4)
+            j.invert_xaxis()
+            j.set_xlabel("ppm")
+
     fig.suptitle("fake vs real comparison")
 
     plt.show()